a478cc0c85b41a0b79a19750bc2014b279674942
Jarvis Potter <jarviskpotter@gmail.com>
2018-05-27 16:04:02 -0400

    update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
 	// if it dosen't recognize the route
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
